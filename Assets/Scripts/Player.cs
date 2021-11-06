b3ffd404f3489524325c952197c1d588fde50945
MCS126 <MCS126@MCS126LAB21>
2021-11-06 17:01:27 -0500

    working on pop up dialog box

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+
+    //private Projectile prefab;
+    private Rigidbody2D body;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameTime.isPaused)
+            return;
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput != 0)
+        {
+            body.velocity = new Vector2(horizontalInput * speed, 0);
+        }
+    }
+}
